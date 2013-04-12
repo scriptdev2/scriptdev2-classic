c265f6f55250a76611e69cebcc16757dc45412b7
cala <calaftp@free.fr>
2013-04-13 01:51:53 +0300

    [z2666] Add support for escort quest 945
    Signed-off-by: Schmoozerd <schmoozerd@scriptdev2.com>
    
    (based on commit 5dd00d5)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>
    
    (based on commit SD2-TBC[s2720] - d342a76)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- sd2_revision_nr.h
+++ sd2_revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __SD2_REVISION_NR_H__
 #define __SD2_REVISION_NR_H__
- #define SD2_REVISION_NR "z2665"
+ #define SD2_REVISION_NR "z2666"
 #endif // __SD2_REVISION_NR_H__
--- sql/scriptdev2_script_full.sql
+++ sql/scriptdev2_script_full.sql
@@ -887,7 +887,26 @@ INSERT INTO script_texts (entry,content_default,sound,type,language,emote,commen
 (-1000887,'End him...',0,0,0,0,'lexlort SAY_LEXLORT_3'),
 (-1000888,'You, soldier, report back to Kargath at once!',0,0,0,1,'lexlort SAY_LEXLORT_4'),
 (-1000889,'%s submits.',0,2,0,0,'grark EMOTE_SUBMIT'),
-(-1000890,'You have come to play? Then let us play!',0,0,0,0,'grark SAY_AGGRO');
+(-1000890,'You have come to play? Then let us play!',0,0,0,0,'grark SAY_AGGRO'),
+
+(-1000891,'REUSE_ME',0,0,0,0,'REUSE_ME'),
+(-1000892,'REUSE_ME',0,0,0,0,'REUSE_ME'),
+(-1000893,'REUSE_ME',0,0,0,0,'REUSE_ME'),
+(-1000894,'REUSE_ME',0,0,0,0,'REUSE_ME'),
+(-1000895,'REUSE_ME',0,0,0,0,'REUSE_ME'),
+(-1000896,'REUSE_ME',0,0,0,0,'REUSE_ME'),
+(-1000897,'REUSE_ME',0,0,0,0,'REUSE_ME'),
+(-1000898,'REUSE_ME',0,0,0,0,'REUSE_ME'),
+(-1000899,'REUSE_ME',0,0,0,0,'REUSE_ME'),
+(-1000900,'REUSE_ME',0,0,0,0,'REUSE_ME'),
+
+(-1000901,'REUSE_ME',0,0,0,0,'REUSE_ME'),
+(-1000902,'REUSE_ME',0,0,0,0,'REUSE_ME'),
+(-1000903,'REUSE_ME',0,0,0,0,'REUSE_ME'),
+(-1000904,'REUSE_ME',0,0,0,0,'REUSE_ME'),
+
+(-1000905,'Ok, let\'s go!!',0,0,0,1,'therylune SAY_THERYLUNE_START'),
+(-1000906,'I can make it the rest of the way. $N. THANKS!',0,0,0,1,'therylune SAY_THERYLUNE_START');
 
 -- -1 033 000 SHADOWFANG KEEP
 INSERT INTO script_texts (entry,content_default,sound,type,language,emote,comment) VALUES
@@ -3111,5 +3130,29 @@ INSERT INTO script_waypoint VALUES
 (9023, 56, 470.39, -6.01, -70.10, 0, ''),
 (9023, 57, 452.45, 29.85, -70.37, 1500, 'SAY_WINDSOR_FREE_1'),
 (9023, 58, 452.45, 29.85, -70.37, 15000, 'SAY_WINDSOR_FREE_2');
+
+DELETE FROM script_waypoint WHERE entry=3584;
+INSERT INTO script_waypoint VALUES
+(3584, 0, 4520.4, 420.235, 33.5284, 2000, ''),
+(3584, 1, 4512.26, 408.881, 32.9308, 0, ''),
+(3584, 2, 4507.94, 396.47, 32.9476, 0, ''),
+(3584, 3, 4507.53, 383.781, 32.995, 0, ''),
+(3584, 4, 4512.1, 374.02, 33.166, 0, ''),
+(3584, 5, 4519.75, 373.241, 33.1574, 0, ''),
+(3584, 6, 4592.41, 369.127, 31.4893, 0, ''),
+(3584, 7, 4598.55, 364.801, 31.4947, 0, ''),
+(3584, 8, 4602.76, 357.649, 32.9265, 0, ''),
+(3584, 9, 4597.88, 352.629, 34.0317, 0, ''),
+(3584, 10, 4590.23, 350.9, 36.2977, 0, ''),
+(3584, 11, 4581.5, 348.254, 38.3878, 0, ''),
+(3584, 12, 4572.05, 348.059, 42.3539, 0, ''),
+(3584, 13, 4564.75, 344.041, 44.2463, 0, ''),
+(3584, 14, 4556.63, 341.003, 47.6755, 0, ''),
+(3584, 15, 4554.38, 334.968, 48.8003, 0, ''),
+(3584, 16, 4557.63, 329.783, 49.9532, 0, ''),
+(3584, 17, 4563.32, 316.829, 53.2409, 0, ''),
+(3584, 18, 4566.09, 303.127, 55.0396, 0, ''),
+(3584, 19, 4561.65, 295.456, 57.0984, 4000, 'SAY_THERYLUNE_FINISH'),
+(3584, 20, 4551.03, 293.333, 57.1534, 2000, '');
 
 -- EOF
