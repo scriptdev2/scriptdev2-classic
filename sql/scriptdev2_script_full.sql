09e9472242a3c000db6122eb487653bad24e6382
Schmoozerd <schmoozerd@scriptdev2.com>
2013-06-04 16:01:40 +0200

    [z2676] Update to CMaNGOS [z2383]
    This requires and fixes compile for CMaNGOS [z2383]
    
    Also use the generic text storage now to load the data and display the texts
    
    Signed-off-by: Schmoozerd <schmoozerd@scriptdev2.com>
    
    (based on commit SD2-TBC[s2747] - c30c4b7)
    
    Signed-off-by: Schmoozerd <schmoozerd@scriptdev2.com>

--- sql/scriptdev2_script_full.sql
+++ sql/scriptdev2_script_full.sql
@@ -3,7 +3,7 @@
 --
 
 DELETE FROM sd2_db_version;
-INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for CMaNGOS classic z2375+) ');
+INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for CMaNGOS classic z2383+) ');
 
 --
 -- Below contains data for table `script_texts` mainly used in C++ parts.
