c53b0d6bf30a4176adc9c8ba6f8d645261adad99
Xfurry <xfurry@scriptdev2.com>
2014-04-13 21:39:39 +0200

    [z2693] Fix build for CMaNGOS z2451
    Require CMaNGOS [z2451+]
    
    (based on commit SD2[2993] - 3dc20b1)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>
    
    (based on commit SD2-TBC[s2777] - 5389b0f)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- sd2_revision_nr.h
+++ sd2_revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __SD2_REVISION_NR_H__
 #define __SD2_REVISION_NR_H__
- #define SD2_REVISION_NR "z2692"
+ #define SD2_REVISION_NR "z2693"
 #endif // __SD2_REVISION_NR_H__
--- sql/scriptdev2_script_full.sql
+++ sql/scriptdev2_script_full.sql
@@ -3,7 +3,7 @@
 --
 
 DELETE FROM sd2_db_version;
-INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for CMaNGOS classic z2425+) ');
+INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for CMaNGOS classic z2451+) ');
 
 --
 -- Below contains data for table `script_texts` mainly used in C++ parts.
