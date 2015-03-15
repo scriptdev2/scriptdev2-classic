55dbc9abf5fecb708ce4fd44b75dbd29a7403514
Schmoozerd <schmoozerd@scriptdev2.com>
2015-03-16 00:07:50 +0100

    [z2730] Use WaypointMovement for Escort-Scripting. Requires CMaNGOS [z2635]
    (based on commit SD2[3097] - 3a8d8e1)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>
    
    (based on commit SD2-TBC[s2831] - 61b7a1e)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- sql/scriptdev2_script_full.sql
+++ sql/scriptdev2_script_full.sql
@@ -3,7 +3,7 @@
 --
 
 DELETE FROM sd2_db_version;
-INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for CMaNGOS classic z2604+) ');
+INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for CMaNGOS classic z2635+) ');
 
 --
 -- Below contains data for table `script_texts` mainly used in C++ parts.
