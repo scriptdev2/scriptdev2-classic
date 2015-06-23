7722c866daa85d3521a478c3c050cec362097394
Tobschinski <tobschinski@gmail.com>
2015-06-23 18:50:55 +0200

    BRD - General Angerforge

---
+++ sql/updates/rXXXX_scriptdev2.sql
@@ -0,0 +1,3 @@
+
+DELETE FROM `script_texts` WHERE `entry`='-1999926';
+INSERT INTO `script_texts` (`entry`, `content_default`, `sound`, `type`, `language`, `emote`, `comment`) VALUES ('-1999926', '%s cries out an alarm!', '0', '2', '0', '0', 'General Angerforge- Alarm');
